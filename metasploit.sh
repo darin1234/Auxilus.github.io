1cf617bd875374db65151d2c748cf25254414a83
Auxilus <raghavsphadke@gmail.com>
2017-09-05 13:26:37 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,35 +1,23 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
 
-echo "installing Prequisities \n"
 apt update
 apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev \
     postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config \
     postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar
 
-echo "\n"
-echo "cloning Metasploit framework\n"
-cd $HOME
-#wget https://github.com/Auxilus/Auxilus.github.io/blob/master/4.14.21.tar.gz
-#tar -xf 4.14.21.tar.gz 
+cd $PREFIX/share/
 curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.2.tar.gz
-tar -xf 4.16.4.tar.gz
-mv metasploit-framework-4.16.4 metasploit-framework
-#git clone https://github.com/rapid7/metasploit-framework --depth 1
-cd metasploit-framework
+tar -xf $PREFIX/share/4.16.4.tar.gz
+mv $PREFIX/share/metasploit-framework-4.16.4 $PREFIX/share/metasploit-framework
+cd $PREFIX/share/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
-echo "\n"
-echo "Installing bundler"
 gem install bundler
 
-echo "\n"
-echo "Installing nokogiri\n"
+
 gem install nokogiri -- --use-system-libraries
 
-
-echo "\n"
-echo "Installing grpc"
-sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
+sed 's|grpc (.*|grpc (1.4.1)|g' -i $PREFIX/share/metasploit-framework/Gemfile.lock
 gem unpack grpc -v 1.4.1
 cd grpc-1.4.1
 curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
@@ -40,16 +28,9 @@ gem install grpc-1.4.1.gem
 cd ..
 rm -r grpc-1.4.1
 
-echo "\n"
-echo "Installig gems\n"
-cd $HOME/metasploit-framework
+
+cd $PREFIX/share/metasploit-framework
 bundle install -j5
 
-
-echo "\n"
-echo "Performing shebang fix"
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-
-echo "\n"
-echo "Type ./msfconsole to start metasploit"
-cd $HOME/metasploit-framework
+ln -s $PREFIX/share/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
