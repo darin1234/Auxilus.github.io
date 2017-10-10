8e45fd4086a1fb4ec3ee66acbd7dbab30a83ff63
Auxilus <raghavsphadke@gmail.com>
2017-10-10 07:59:59 +0530

    c

--- r.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bash
-
-while read p; do
-  gem install $p
-done < gemdeps
--- rr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#wget https://Auxilus.github.io/gemfile
-while IFS='' read -r line || [[  "$line" ]]; do
-    echo "Installing $line \n" 
-    #gem install $line
-done < "gemdeps"
