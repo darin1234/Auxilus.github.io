b55d5773a225427406dd0e09600faaa26e8ac00c
Auxilus <raghavsphadke@gmail.com>
2017-11-12 09:06:10 +0530

    cleanup

--- Apktool_build.sh
+++
@@ -1,8 +0,0 @@
-TERMUX_PKG_HOMEPAGE=hhttps://www.kernel.org/pub/software/network/ethtool/
-TERMUX_PKG_DESCRIPTION="Tool to compile and decompile apk"
-# TERMUX_PKG_VERSION= Not Required 
-TERMUX_PKG_SRCURL=https://Hax4us.github.io/Apktool.tar.gz
-TERMUX_PKG_SHA256=af2fd9692f3159d3ab1e41e6f9b7d8db2a4693f1cb22348c88ba89f70f0e6503
-TERMUX_PKG_DEPENDS="libnl, libnl-dev"
-TERMUX_PKG_BUILD_IN_SRC=yes
-
